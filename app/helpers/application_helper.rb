cec4e76374bf70846053373e0079869714554544
Yann Beilliard <yann.beilliard@gmail.com>
2017-12-21 20:54:11 +0000

    Category Navigation Responsive System Added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,18 @@ module ApplicationHelper
     def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
     end
+    
+    def category_icon(category)
+        case category
+        when "Physics" then "fab fa-react"
+        when "Chemistry" then "fas fa-flask"
+        when "Biology" then "fab fa-pagelines"
+        when "Cosmology" then "fab fa-superpowers"
+        when "Computer Science" then "fas fa-microchip"
+        when "Technology" then "fas fa-code-branch"
+        when "Mathematics" then "fas fa-chart-area"
+        when "Aerospace" then "fas fa-rocket"
+        when "Health" then "fas fa-medkit"
+        end
+    end
 end
